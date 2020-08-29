2ad89d7b2d65dea070f3f5dbfffb7b349efae86f
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-08-29 19:14:30 +0530

    Added:Test case to check to numberof the records incorrect

--- IPL Analyser/src/test/java/IPLAnalyserTest.java
+++ IPL Analyser/src/test/java/IPLAnalyserTest.java
@@ -3,18 +3,25 @@ import cricketleagueanalysis.IPLAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
 
-    public class IPLAnalyserTest {
+public class IPLAnalyserTest {
 
-        private static final String IPL_BATSMAN_CSV_FILE_PATH = "./src/test/resources/IPLBat.csv";
-        private static final String IPL_BATSMAN_CSV_WRONG_FILE_PATH = "./src/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_BATSMAN_CSV_FILE_PATH = "./src/test/resources/IPLBat.csv";
+    private static final String IPL_BATSMAN_CSV_WRONG_FILE_PATH = "./src/resources/IPL2019FactsheetMostRuns.csv";
 
-
-        @Test
-        public void givenIPLFactSheetMostRunsFile_whenProper_shouldReturnCorrectRecordCount() throws IPLAnalyserException {
-            IPLAnalyser iplAnalyser = new IPLAnalyser();
-            int numOfRecords = iplAnalyser.loadIPLFactSheetData(IPLAnalyser.PlayerType.BATSMAN, IPL_BATSMAN_CSV_FILE_PATH);
-            Assert.assertEquals(100, numOfRecords);
-            System.out.println(numOfRecords);
-        }
+    @Test
+    public void givenIPLFactSheetMostRunsFile_whenProper_shouldReturnCorrectRecordCount() throws IPLAnalyserException {
+        IPLAnalyser iplAnalyser = new IPLAnalyser();
+        int numOfRecords = iplAnalyser.loadIPLFactSheetData(IPLAnalyser.PlayerType.BATSMAN, IPL_BATSMAN_CSV_FILE_PATH);
+        Assert.assertEquals(100, numOfRecords);
+        System.out.println(numOfRecords);
     }
+
+    @Test
+    public void givenIPLFactSheetMostRunsDatafile_whenNotProper_shouldReturnInCorrectRecordCount() throws IPLAnalyserException {
+        IPLAnalyser iplAnalyser = new IPLAnalyser();
+        int numOfRecords = iplAnalyser.loadIPLFactSheetData(IPLAnalyser.PlayerType.BATSMAN, IPL_BATSMAN_CSV_FILE_PATH);
+        Assert.assertNotEquals(99, numOfRecords);
+    }
+}
+
 
