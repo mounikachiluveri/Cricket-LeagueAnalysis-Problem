e9128a603917bc25f10da9254436ed518a3122a3
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-08-30 00:24:17 +0530

    Added: code to Test to find out the top BowlingAverage person

--- IPL Analyser/src/main/java/cricketleagueanalysis/SortBy.java
+++ IPL Analyser/src/main/java/cricketleagueanalysis/SortBy.java
@@ -9,7 +9,7 @@ import java.util.Map;
     public class SortBy{
         static Map<Field, Comparator> sortFieldComparator = new HashMap<>();
         public enum Field {
-            AVERAGE_OF_BATSMAN,STRIKE_RATE,FOURS_SIXES_RATE,FOURS_SIXES_WITH_STRIKE_RATE,AVERAGE_OF_BATSMAN_WITH_STRIKE_RATE,RUNS_WITH_AVERAGE;
+            AVERAGE_OF_BATSMAN,STRIKE_RATE,FOURS_SIXES_RATE,FOURS_SIXES_WITH_STRIKE_RATE,AVERAGE_OF_BATSMAN_WITH_STRIKE_RATE,RUNS_WITH_AVERAGE,BOWLING_AVERAGE;
         }
 
         public static Comparator getComparatorField(SortBy.Field field) {
@@ -17,12 +17,14 @@ import java.util.Map;
             Comparator<IPLDAO> iplStrikeRateComparator = Comparator.comparing(census -> census.strikeRate);
             Comparator<IPLDAO> iplfoursandsixesComparator = Comparator.comparing(census -> census.sixes*6 +census.fours*4);
             Comparator<IPLDAO> iplrunsComparator = Comparator.comparing(census -> census.runs);
+            Comparator<IPLDAO> iplbowlingComparator = Comparator.comparing(census -> census.bowlingAverage);
             sortFieldComparator.put(Field.AVERAGE_OF_BATSMAN, iplAverageBattingComparator.reversed());
             sortFieldComparator.put(Field.STRIKE_RATE, iplStrikeRateComparator.reversed());
             sortFieldComparator.put(Field.FOURS_SIXES_RATE, iplfoursandsixesComparator.reversed());
             sortFieldComparator.put(Field.FOURS_SIXES_WITH_STRIKE_RATE, iplfoursandsixesComparator.thenComparing(iplStrikeRateComparator).reversed());
             sortFieldComparator.put(Field.AVERAGE_OF_BATSMAN_WITH_STRIKE_RATE, iplStrikeRateComparator.thenComparing(iplAverageBattingComparator).reversed());
             sortFieldComparator.put(Field.RUNS_WITH_AVERAGE, iplrunsComparator.thenComparing(iplAverageBattingComparator).reversed());
+            sortFieldComparator.put(Field.BOWLING_AVERAGE, iplbowlingComparator.reversed());
             return (Comparator<IPLDAO>) sortFieldComparator.get(field);
         }
     }
